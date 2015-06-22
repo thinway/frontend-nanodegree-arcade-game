737bde39af875b2d0341a3fce3a7520f18373aeb
Fran Thinway <thinway@icloud.com>
2015-06-22 11:19:03 +0200

    Added the game logic

--- js/app.js
+++ js/app.js
@@ -6,7 +6,14 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-}
+
+    // The enemy coordinates
+    this.x = 0;
+    this.y = 0;
+
+    // Its speed
+    this.speed;
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -14,23 +21,134 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-}
+
+    this.x += this.speed * dt;
+
+    if ( this.x > 490 ) {
+        this.x = -100;
+    };
+};
+
+// init method for bugs. Called from initializedEnemies()
+Enemy.prototype.init = function(row, speed, enemy) {
+    this.x = -350 * enemy; // Keep enemies on the same row apart enough from each other 
+    this.y = row;
+    this.speed = speed;
+    allEnemies.push(this); 
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    // Load a random player's sprite
+    var sprites = [
+        'images/char-boy.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/char-princess-girl.png'
+    ];
 
+    this.sprite = sprites[randomInt(0,sprites.length - 1)];
+
+    // Player's coordinates
+    this.reset();
+};
+
+// When the player is killed by a bug or reaches the water
+Player.prototype.reset = function() {
+    this.x = 200;
+    this.y = 320;
+};
+
+Player.prototype.update = function(dt) {
+
+    // First of all check if the player has reached the water or any limit of the cavas
+    if (this.y < 0) {
+        alert("Cool!!! You've reached the fresh water!");
+        // so return to the start position
+        this.reset();
+    } else if (this.x > 402) {
+         // Don't go beyond the right limit 
+        this.x = 402;
+    } else if (this.y > 320) {
+        // Don't go beyond the bottom limit
+        this.y = 391;
+    } else if (this.x < -2) {
+        // Don't go beyond the left limit
+        this.x = -2;
+    }
+
+    // And next, check if the player has been reached by a bug
+    var index = 0;
+    for (index = 0; index < allEnemies.length; index++) {
+        // Set up the limits of the enemy
+        upLimit = allEnemies[index].y + 50;
+        rightLimit = allEnemies[index].x + 50;
+        bottomLimit = allEnemies[index].y - 50;
+        leftLimit = allEnemies[index].x - 50;
+        
+        // And check if the player is inside that limit
+        if (this.y < upLimit && this.x < rightLimit &&  this.x > leftLimit && this.y > bottomLimit ) {
+            alert("Nooo, feel the jaws of that ladybird!!");
+            // In that case, the player dies and returns to the start position
+            this.reset();
+        }
+    }
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(userInput) {
+    if (userInput == "up") {
+        this.y = this.y - 83;
+    } else if (userInput == "right") {
+        this.x = this.x + 101;
+    } else if (userInput == "down") {
+        this.y = this.y + 83;
+    }  else if (userInput == "left") {
+        this.x = this.x - 101;
+    }
+    // Debug: the player's coordinates
+    console.log("x,y = " + this.x + "," + this.y);
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+// The vertical coordinate of each pavement row on the canvas
+var rows = [60,145,230];
 
+var allEnemies = [];
+
+// Create the bugs army
+function initEnemies() {
+    var index = 0;
+
+    while ( index < rows.length ) {
+        var speed = randomInt(200, 400);
+        var numEnemiesRow = 1;
+        for (var aEnemy = 0; aEnemy < numEnemiesRow; aEnemy++) {
+            var enemy = new Enemy();
+            enemy.init(rows[index], speed, aEnemy);
+        }
+        index++;
+    }
+};
+
+// Creating the enemies
+initEnemies();
+
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -44,3 +162,9 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+// Generate a random int between min and max. Both included.
+function randomInt(min, max) {
+    max++;
+    return Math.floor(Math.random() * (max - min)) + min;
+};
